e789ba01f8843a04014bd59f430e8e9cb999cd6f
unknown <eypcnmrt@gmail.com>
2024-04-22 16:41:38 +0300

    ilk versiyon

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import Home from "./components/Home";
+import User from "./components/user/User";
+
+export const routes = [
+  {
+    path: "",
+    component: Home,
+  },
+  {
+    path: "/user",
+    component: User,
+  },
+];
